7e3c13bc4bd4d7d75be62464a5a056a0c13b7881
nikuya3 <nikuya@dummy.mail>
2017-06-09 16:51:15 +0200

    Tried to improve the calculations using multiple inputs

--- net.py
+++ net.py
@@ -24,7 +24,7 @@ def get_data():
         else:
             x_data = np.concatenate((x_data, batch[b'data']), axis=0)
             y_data += batch[b'labels']
-    return x_data[:1000], y_data[:1000]
+    return x_data[80:120], y_data[80:120]
 
 
 def split_data(data, train, val, test):
@@ -94,9 +94,9 @@ def data_loss(s, y, delta):
     #x = np.sum(np.maximum(0, s - s[y] + delta))
     for i in range(len(y)):
         y_i = y[i]
-        for j in range(len(s[i])):
+        for j in range(len(s[i][0])):
             if j != y_i:
-                loss += max(0, s[i][j] - s[i][y_i] + delta)
+                loss += max(0, s[i][0][j] - s[i][0][y_i] + delta)
     return loss / len(s)
 
 
@@ -134,18 +134,18 @@ def loss_gradient_by_scores(s, y, delta):
     """
     for i in range(len(y)):
         y_i = y[i]
-        for j in range(len(s[i])):
-            if s[i][j] - s[i][y_i] + delta < 0:
-                s[i][j] = 0
+        for j in range(len(s[i][0])):
+            if s[i][0][j] - s[i][0][y_i] + delta < 0:
+                s[i][0][j] = 0
             else:
-                s[i][j] = 1
+                s[i][0][j] = 1
     return s
 
 
 # hyperparameters
 delta = 1  # Data loss parameter
-lambda_ = .01  # The regularization strength (has an influence on regularization loss).
-learning_rate = .001  # The step size for each epoch (influences how greedy the network changes its parameters)
+lambda_ = 0  # The regularization strength (has an influence on regularization loss).
+learning_rate = .01  # The step size for each epoch (influences how greedy the network changes its parameters)
 epochs = 100  # The amount of iterations the network should take
 
 # Input data: 80 % train, 10 % val, 10 % test
@@ -159,36 +159,36 @@ x_tr, x_val, x_te, pre_mean, pre_std = preprocess_data(x_tr, x_val, x_te)
 
 # Neural net: IN (3072 x 1) -> HL (100 x 100) -> HL (100 x 1) -> OUT (10 x 1)
 k = len(np.unique(y_data))  # number of classes
-hidden1_shape = [50000, 50000]
+hidden1_shape = [1000, 1000]
 hidden2_shape = [100, 1]
 hidden_shapes = [hidden1_shape, hidden2_shape]
 out_shape = [k, 1]
 
 # weight initialization (specialized for relu activations)
 w_hidden1 = np.random.randn(x_tr[0].shape[0], hidden1_shape[0]) * sqrt(2 / x_tr[0].shape[0])
-w_hidden2 = np.random.randn(hidden1_shape[1], hidden2_shape[0]) * sqrt(2 / w_hidden1.shape[1])
-w_out = np.random.randn(hidden2_shape[0], out_shape[0]) * sqrt(2 / w_hidden2.shape[1])
+w_hidden2 = np.random.randn(hidden1_shape[1], hidden2_shape[0]) * sqrt(2 / x_tr[0].shape[0])
+w_out = np.random.randn(hidden2_shape[0], out_shape[0]) * sqrt(2 / x_tr[0].shape[0])
 
 b_hidden1 = np.zeros((1, hidden1_shape[0]))
 b_hidden2 = np.zeros((1, hidden2_shape[0]))
 b_out = np.zeros((1, out_shape[0]))
 
 for epoch in range(epochs):
-    outs = np.empty((0, out_shape[0]))
-    hiddens_1 = np.empty((0, hidden1_shape[0]))
-    hiddens_2 = np.empty((0, hidden2_shape[0]))
+    outs = []
+    hiddens_1 = []
+    hiddens_2 = []
     # forward pass
     for row in x_tr:
         input_layer = row
         hidden1 = input_layer.dot(w_hidden1) + b_hidden1
         hidden1 = calculate_activation(hidden1)
-        hiddens_1 = np.vstack((hiddens_1, hidden1))
+        hiddens_1.append(hidden1)
         # ToDo: Apply dropout
         hidden2 = hidden1.dot(w_hidden2) + b_hidden2
         hidden2 = calculate_activation(hidden2)
-        hiddens_2 = np.vstack((hiddens_2, hidden2))
+        hiddens_2.append(hidden2)
         out = hidden2.dot(w_out) + b_out
-        outs = np.vstack((outs, out))
+        outs.append(out)
 
 
     # Calculate loss
@@ -198,28 +198,36 @@ for epoch in range(epochs):
     # Backpropagation
 
     dscores = loss_gradient_by_scores(outs, y_tr, delta)
+    dw_out = np.empty(w_out.shape)
+    db_out = np.empty(b_out.shape)
+    dw_hidden1 = np.empty(w_hidden1.shape)
+    db_hidden1 = np.empty(b_hidden1.shape)
+    dw_hidden2 = np.empty(w_hidden2.shape)
+    db_hidden2 = np.empty(b_hidden2.shape)
 
-    dw_out = hiddens_2.T.dot(dscores)
-    db_out = np.sum(dscores, axis=0, keepdims=True)
-    dhidden2 = dscores.dot(w_out.T)
-    dhidden2[dhidden2 < 0] = 0
+    for i in range(len(hiddens_1)):
+        ds = dscores[i]
+        dw_out += hiddens_2[i].T.dot(ds)
+        db_out += np.sum(ds, axis=0, keepdims=True)
+        dhidden2 = ds.dot(w_out.T)
+        dhidden2[hiddens_2[i] < 0] = 0
 
-    dw_hidden2 = hiddens_1.T.dot(dhidden2)
-    db_hidden2 = np.sum(dhidden2, axis=0, keepdims=True)
-    dhidden1 = dhidden2.dot(w_hidden2.T)
-    dhidden1[dhidden1 < 0] = 0
+        dw_hidden2 += hiddens_1[i].T.dot(dhidden2)
+        db_hidden2 += np.sum(dhidden2, axis=0, keepdims=True)
+        dhidden1 = dhidden2.dot(w_hidden2.T)
+        dhidden1[hiddens_1[i] < 0] = 0
 
-    dw_hidden1 = x_tr.T.dot(dhidden1)
-    db_hidden1 = np.sum(dhidden1, axis=0, keepdims=True)
+        dw_hidden1 += x_tr[i].reshape(1, 3072).T.dot(dhidden1)
+        db_hidden1 += np.sum(dhidden1, axis=0, keepdims=True)
 
     # Set weights using gradients of backpropagation
 
-    w_hidden1 += - learning_rate * dw_hidden1
-    w_hidden2 += - learning_rate * dw_hidden2
-    w_out += - learning_rate * dw_out
-    b_hidden1 += - learning_rate * db_hidden1
-    b_hidden2 += - learning_rate * db_hidden2
-    b_out += - learning_rate * db_out
+    w_hidden1 += - learning_rate * dw_hidden1 / len(hiddens_1)
+    w_hidden2 += - learning_rate * dw_hidden2 / len(hiddens_1)
+    w_out += - learning_rate * dw_out / len(hiddens_1)
+    b_hidden1 += - learning_rate * db_hidden1 / len(hiddens_1)
+    b_hidden2 += - learning_rate * db_hidden2 / len(hiddens_1)
+    b_out += - learning_rate * db_out / len(hiddens_1)
 
 with open('dump.p', 'wb') as dump_file:
     dump(w_hidden1, dump_file)
